59cbaa5df5ad4761c149b932720222d502c24d1d
Kuripipeer <mchristianaxel@gmail.com>
2025-12-04 15:40:18 -0600

    Changes in Routes y navController in HomeScreen

--- app/src/main/java/com/example/cameratest/MainActivity.kt
+++ app/src/main/java/com/example/cameratest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cameratest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -14,6 +15,7 @@ import com.example.cameratest.ui.theme.CameraTestTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             CameraTestTheme {
                 val navController = rememberNavController()
